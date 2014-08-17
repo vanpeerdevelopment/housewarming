8cdb692da23cf52d8a8e02f5f7dd05e035a59cab
Development Van Peer <dieter@vanpeerdevelopment.be>
2014-08-17 15:56:16 +0200

    angularJS setup

---
+++ app/app.js
@@ -0,0 +1,12 @@
+var app = angular.module('housewarmingApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			controller: 'HomeController',
+			templateUrl: '/app/partials/home.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		})
+});
